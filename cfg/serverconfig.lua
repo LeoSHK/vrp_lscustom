9c04e625341d02b69a4e3c7287c1ec905830e2d2
LeoSHK <leonardosilva.r@hotmail.com>
2019-01-25 13:46:23 -0200

    Add files via upload

---
+++ cfg/serverconfig.lua
@@ -0,0 +1,483 @@
+local cfg = {}
+cfg.mods = {
+    ----------Liveries--------
+  [48] = {
+    startprice = 15000,
+    increaseby = 2500
+  },
+
+  ----------Windows--------
+  [46] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Tank--------
+  [45] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Trim--------
+  [44] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Aerials--------
+  [43] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Arch cover--------
+  [42] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Struts--------
+  [41] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Air filter--------
+  [40] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Engine block--------
+  [39] = {
+    startprice = 5000,
+    increaseby = 10250
+  },
+
+  ----------Hydraulics--------
+  [38] = {
+    startprice = 15000,
+    increaseby = 2500
+  },
+
+  ----------Trunk--------
+  [37] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Speakers--------
+  [36] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Plaques--------
+  [35] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Shift leavers--------
+  [34] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Steeringwheel--------
+  [33] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Seats--------
+  [32] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Door speaker--------
+  [31] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Dial--------
+  [30] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+  ----------Dashboard--------
+  [29] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Ornaments--------
+  [28] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Trim--------
+  [27] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Vanity plates--------
+  [26] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ----------Plate holder--------
+  [25] = {
+    startprice = 5000,
+    increaseby = 1250
+  },
+
+  ---------Headlights---------
+  [22] = {
+    {name = "Stock Lights", mod = 0, price = 0},
+    {name = "Xenon Lights", mod = 1, price = 1625},
+  },
+
+  ----------Turbo---------
+  [18] = {
+    { name = "None", mod = 0, price = 0},
+    { name = "Turbo Tuning", mod = 1, price = 20000},
+  },
+
+  -----------Armor-------------
+  [16] = {
+    {name = "Armor Upgrade 20%",modtype = 16, mod = 0, price = 25000},
+    {name = "Armor Upgrade 40%",modtype = 16, mod = 1, price = 50000},
+    {name = "Armor Upgrade 60%",modtype = 16, mod = 2, price = 70000},
+    {name = "Armor Upgrade 80%",modtype = 16, mod = 3, price = 100000},
+    {name = "Armor Upgrade 100%",modtype = 16, mod = 4, price = 120000},
+  },
+
+  ---------Suspension-----------
+  [15] = {
+    {name = "Lowered Suspension",mod = 0, price = 8000},
+    {name = "Street Suspension",mod = 1, price = 9700},
+    {name = "Sport Suspension",mod = 2, price = 10100},
+    {name = "Competition Suspension",mod = 3, price = 11200},
+  },
+
+  -----------Horn----------
+  [14] = {
+    {name = "Truck Horn", mod = 0, price = 1625},
+    {name = "Police Horn", mod = 1, price = 4062},
+    {name = "Clown Horn", mod = 2, price = 6500},
+    {name = "Musical Horn 1", mod = 3, price = 7000},
+    {name = "Musical Horn 2", mod = 4, price = 7000},
+    {name = "Musical Horn 3", mod = 5, price = 7000},
+    {name = "Musical Horn 4", mod = 6, price = 7000},
+    {name = "Musical Horn 5", mod = 7, price = 7000},
+    {name = "Sadtrombone Horn", mod = 8, price = 7000},
+    {name = "Calssical Horn 1", mod = 9, price = 7000},
+    {name = "Calssical Horn 2", mod = 10, price = 7000},
+    {name = "Calssical Horn 3", mod = 11, price = 7000},
+    {name = "Calssical Horn 4", mod = 12, price = 7000},
+    {name = "Calssical Horn 5", mod = 13, price = 7000},
+    {name = "Calssical Horn 6", mod = 14, price = 7000},
+    {name = "Calssical Horn 7", mod = 15, price = 7000},
+    {name = "Scaledo Horn", mod = 16, price = 7000},
+    {name = "Scalere Horn", mod = 17, price = 7000},
+    {name = "Scalemi Horn", mod = 18, price = 7000},
+    {name = "Scalefa Horn", mod = 19, price = 7000},
+    {name = "Scalesol Horn", mod = 20, price = 7000},
+    {name = "Scalela Horn", mod = 21, price = 7000},
+    {name = "Scaleti Horn", mod = 22, price = 7000},
+    {name = "Scaledo Horn High", mod = 23, price = 7000},
+    {name = "Jazz Horn 1", mod = 25, price = 7000},
+    {name = "Jazz Horn 2", mod = 26, price = 7000},
+    {name = "Jazz Horn 3", mod = 27, price = 7000},
+    {name = "Jazzloop Horn", mod = 28, price = 7000},
+    {name = "Starspangban Horn 1", mod = 29, price = 7000},
+    {name = "Starspangban Horn 2", mod = 30, price = 7000},
+    {name = "Starspangban Horn 3", mod = 31, price = 7000},
+    {name = "Starspangban Horn 4", mod = 32, price = 7000},
+    {name = "Classicalloop Horn 1", mod = 33, price = 7000},
+    {name = "Classicalloop Horn 2", mod = 34, price = 7000},
+    {name = "Classicalloop Horn 3", mod = 35, price = 7000},
+  },
+
+  ----------Transmission---------
+  [13] = {
+    {name = "Street Transmission", mod = 0, price = 10000},
+		{name = "Sports Transmission", mod = 1, price = 15200},
+		{name = "Race Transmission", mod = 2, price = 25000},
+  },
+
+  -----------Brakes-------------
+  [12] = {
+    {name = "Street Brakes", mod = 0, price = 9500},
+		{name = "Sport Brakes", mod = 1, price = 12775},
+		{name = "Race Brakes", mod = 2, price = 21000},
+  },
+
+  ------------Engine----------
+  [11] = {
+    {name = "EMS Upgrade, Level 2", mod = 0, price = 15000},
+    {name = "EMS Upgrade, Level 3", mod = 1, price = 23520},
+    {name = "EMS Upgrade, Level 4", mod = 2, price = 28250},
+  },
+
+  -------------Roof----------
+  [10] = {
+    startprice = 1250,
+    increaseby = 400
+  },
+
+  ------------Fenders---------
+  [8] = {
+    startprice = 1500,
+    increaseby = 400
+  },
+
+  ------------Hood----------
+  [7] = {
+    startprice = 1500,
+    increaseby = 400
+  },
+
+  ----------Grille----------
+  [6] = {
+    startprice = 1250,
+    increaseby = 400
+  },
+
+  ----------Roll cage----------
+  [5] = {
+    startprice = 1250,
+    increaseby = 400
+  },
+
+  ----------Exhaust----------
+  [4] = {
+    startprice = 1000,
+    increaseby = 400
+  },
+
+  ----------Skirts----------
+  [3] = {
+    startprice = 1250,
+    increaseby = 400
+  },
+
+  -----------Rear bumpers----------
+  [2] = {
+    startprice = 2500,
+    increaseby = 500
+  },
+
+  ----------Front bumpers----------
+  [1] = {
+    startprice = 2500,
+    increaseby = 500
+  },
+
+  ----------Spoiler----------
+  [0] = {
+    startprice = 2500,
+    increaseby = 400
+  },
+}
+
+cfg.frontwheel = {
+  {name = "Stock", wtype = 6, mod = -1, price = 500},
+  {name = "Speedway", wtype = 6, mod = 0, price = 500},
+  {name = "Streetspecial", wtype = 6, mod = 1, price = 500},
+  {name = "Racer", wtype = 6, mod = 2, price = 500},
+  {name = "Trackstar", wtype = 6, mod = 3, price = 500},
+  {name = "Overlord", wtype = 6, mod = 4, price = 500},
+  {name = "Trident", wtype = 6, mod = 5, price = 500},
+  {name = "Triplethreat", wtype = 6, mod = 6, price = 500},
+  {name = "Stilleto", wtype = 6, mod = 7, price = 500},
+  {name = "Wires", wtype = 6, mod = 8, price = 500},
+  {name = "Bobber", wtype = 6, mod = 9, price = 500},
+  {name = "Solidus", wtype = 6, mod = 10, price = 500},
+  {name = "Iceshield", wtype = 6, mod = 11, price = 500},
+  {name = "Loops", wtype = 6, mod = 12, price = 500},
+}
+
+----Back wheel (Bikes)-----
+cfg.backwheel = {
+  {name = "Stock", wtype = 6, mod = -1, price = 500},
+  {name = "Speedway", wtype = 6, mod = 0, price = 500},
+  {name = "Streetspecial", wtype = 6, mod = 1, price = 500},
+  {name = "Racer", wtype = 6, mod = 2, price = 500},
+  {name = "Trackstar", wtype = 6, mod = 3, price = 500},
+  {name = "Overlord", wtype = 6, mod = 4, price = 500},
+  {name = "Trident", wtype = 6, mod = 5, price = 500},
+  {name = "Triplethreat", wtype = 6, mod = 6, price = 500},
+  {name = "Stilleto", wtype = 6, mod = 7, price = 500},
+  {name = "Wires", wtype = 6, mod = 8, price = 500},
+  {name = "Bobber", wtype = 6, mod = 9, price = 500},
+  {name = "Solidus", wtype = 6, mod = 10, price = 500},
+  {name = "Iceshield", wtype = 6, mod = 11, price = 500},
+  {name = "Loops", wtype = 6, mod = 12, price = 500},
+}
+----Sport wheels-----
+cfg.sportwheels = {
+  {name = "Stock", wtype = 0, mod = -1, price = 1000},
+  {name = "Inferno", wtype = 0, mod = 0, price = 1000},
+  {name = "Deepfive", wtype = 0, mod = 1, price = 1000},
+  {name = "Lozspeed", wtype = 0, mod = 2, price = 1000},
+  {name = "Diamondcut", wtype = 0, mod = 3, price = 1000},
+  {name = "Chrono", wtype = 0, mod = 4, price = 1000},
+  {name = "Feroccirr", wtype = 0, mod = 5, price = 1000},
+  {name = "Fiftynine", wtype = 0, mod = 6, price = 1000},
+  {name = "Mercie", wtype = 0, mod = 7, price = 1000},
+  {name = "Syntheticz", wtype = 0, mod = 8, price = 1000},
+  {name = "Organictyped", wtype = 0, mod = 9, price = 1000},
+  {name = "Endov1", wtype = 0, mod = 10, price = 1000},
+  {name = "Duper7", wtype = 0, mod = 11, price = 1000},
+  {name = "Uzer", wtype = 0, mod = 12, price = 1000},
+  {name = "Groundride", wtype = 0, mod = 13, price = 1000},
+  {name = "Spacer", wtype = 0, mod = 14, price = 1000},
+  {name = "Venum", wtype = 0, mod = 15, price = 1000},
+  {name = "Cosmo", wtype = 0, mod = 16, price = 1000},
+  {name = "Dashvip", wtype = 0, mod = 17, price = 1000},
+  {name = "Icekid", wtype = 0, mod = 18, price = 1000},
+  {name = "Ruffeld", wtype = 0, mod = 19, price = 1000},
+  {name = "Wangenmaster", wtype = 0, mod = 20, price = 1000},
+  {name = "Superfive", wtype = 0, mod = 21, price = 1000},
+  {name = "Endov2", wtype = 0, mod = 22, price = 1000},
+  {name = "Slitsix", wtype = 0, mod = 23, price = 1000},
+}
+-----Suv wheels------
+cfg.suvwheels = {
+  {name = "Stock", wtype = 3, mod = -1, price = 1000},
+  {name = "Vip", wtype = 3, mod = 0, price = 1000},
+  {name = "Benefactor", wtype = 3, mod = 1, price = 1000},
+  {name = "Cosmo", wtype = 3, mod = 2, price = 1000},
+  {name = "Bippu", wtype = 3, mod = 3, price = 1000},
+  {name = "Royalsix", wtype = 3, mod = 4, price = 1000},
+  {name = "Fagorme", wtype = 3, mod = 5, price = 1000},
+  {name = "Deluxe", wtype = 3, mod = 6, price = 1000},
+  {name = "Icedout", wtype = 3, mod = 7, price = 1000},
+  {name = "Cognscenti", wtype = 3, mod = 8, price = 1000},
+  {name = "Lozspeedten", wtype = 3, mod = 9, price = 1000},
+  {name = "Supernova", wtype = 3, mod = 10, price = 1000},
+  {name = "Obeyrs", wtype = 3, mod = 11, price = 1000},
+  {name = "Lozspeedballer", wtype = 3, mod = 12, price = 1000},
+  {name = "Extra vaganzo", wtype = 3, mod = 13, price = 1000},
+  {name = "Splitsix", wtype = 3, mod = 14, price = 1000},
+  {name = "Empowered", wtype = 3, mod = 15, price = 1000},
+  {name = "Sunrise", wtype = 3, mod = 16, price = 1000},
+  {name = "Dashvip", wtype = 3, mod = 17, price = 1000},
+  {name = "Cutter", wtype = 3, mod = 18, price = 1000},
+}
+-----Offroad wheels-----
+cfg.offroadwheels = {
+  {name = "Stock", wtype = 4, mod = -1, price = 1000},
+  {name = "Raider", wtype = 4, mod = 0, price = 1000},
+  {name = "Mudslinger", wtype = 4, modtype = 23, wtype = 4, mod = 1, price = 1000},
+  {name = "Nevis", wtype = 4, mod = 2, price = 1000},
+  {name = "Cairngorm", wtype = 4, mod = 3, price = 1000},
+  {name = "Amazon", wtype = 4, mod = 4, price = 1000},
+  {name = "Challenger", wtype = 4, mod = 5, price = 1000},
+  {name = "Dunebasher", wtype = 4, mod = 6, price = 1000},
+  {name = "Fivestar", wtype = 4, mod = 7, price = 1000},
+  {name = "Rockcrawler", wtype = 4, mod = 8, price = 1000},
+  {name = "Milspecsteelie", wtype = 4, mod = 9, price = 1000},
+}
+-----Tuner wheels------
+cfg.tunerwheels = {
+  {name = "Stock", wtype = 5, mod = -1, price = 1000},
+  {name = "Cosmo", wtype = 5, mod = 0, price = 1000},
+  {name = "Supermesh", wtype = 5, mod = 1, price = 1000},
+  {name = "Outsider", wtype = 5, mod = 2, price = 1000},
+  {name = "Rollas", wtype = 5, mod = 3, price = 1000},
+  {name = "Driffmeister", wtype = 5, mod = 4, price = 1000},
+  {name = "Slicer", wtype = 5, mod = 5, price = 1000},
+  {name = "Elquatro", wtype = 5, mod = 6, price = 1000},
+  {name = "Dubbed", wtype = 5, mod = 7, price = 1000},
+  {name = "Fivestar", wtype = 5, mod = 8, price = 1000},
+  {name = "Slideways", wtype = 5, mod = 9, price = 1000},
+  {name = "Apex", wtype = 5, mod = 10, price = 1000},
+  {name = "Stancedeg", wtype = 5, mod = 11, price = 1000},
+  {name = "Countersteer", wtype = 5, mod = 12, price = 1000},
+  {name = "Endov1", wtype = 5, mod = 13, price = 1000},
+  {name = "Endov2dish", wtype = 5, mod = 14, price = 1000},
+  {name = "Guppez", wtype = 5, mod = 15, price = 1000},
+  {name = "Chokadori", wtype = 5, mod = 16, price = 1000},
+  {name = "Chicane", wtype = 5, mod = 17, price = 1000},
+  {name = "Saisoku", wtype = 5, mod = 18, price = 1000},
+  {name = "Dishedeight", wtype = 5, mod = 19, price = 1000},
+  {name = "Fujiwara", wtype = 5, mod = 20, price = 1000},
+  {name = "Zokusha", wtype = 5, mod = 21, price = 1000},
+  {name = "Battlevill", wtype = 5, mod = 22, price = 1000},
+  {name = "Rallymaster", wtype = 5, mod = 23, price = 1000},
+}
+-----Highend wheels------
+cfg.highendwheels = {
+  {name = "Stock", wtype = 7, mod = -1, price = 1500},
+  {name = "Shadow", wtype = 7, mod = 0, price = 1500},
+  {name = "Hyper", wtype = 7, mod = 1, price = 1500},
+  {name = "Blade", wtype = 7, mod = 2, price = 1500},
+  {name = "Diamond", wtype = 7, mod = 3, price = 1500},
+  {name = "Supagee", wtype = 7, mod = 4, price = 1500},
+  {name = "Chromaticz", wtype = 7, mod = 5, price = 1500},
+  {name = "Merciechlip", wtype = 7, mod = 6, price = 1500},
+  {name = "Obeyrs", wtype = 7, mod = 7, price = 1500},
+  {name = "Gtchrome", wtype = 7, mod = 8, price = 1500},
+  {name = "Cheetahr", wtype = 7, mod = 9, price = 1500},
+  {name = "Solar", wtype = 7, mod = 10, price = 1500},
+  {name = "Splitten", wtype = 7, mod = 11, price = 1500},
+  {name = "Dashvip", wtype = 7, mod = 12, price = 1500},
+  {name = "Lozspeedten", wtype = 7, mod = 13, price = 1500},
+  {name = "Carboninferno", wtype = 7, mod = 14, price = 1500},
+  {name = "Carbonshadow", wtype = 7, mod = 15, price = 1500},
+  {name = "Carbonz", wtype = 7, mod = 16, price = 1500},
+  {name = "Carbonsolar", wtype = 7, mod = 17, price = 1500},
+  {name = "Carboncheetahr", wtype = 7, mod = 18, price = 1500},
+  {name = "Carbonsracer", wtype = 7, mod = 19, price = 1500},
+}
+-----Lowrider wheels------
+cfg.lowriderwheels = {
+  {name = "Stock", wtype = 2, mod = -1, price = 1500},
+  {name = "Flare", wtype = 2, mod = 0, price = 1500},
+  {name = "Wired", wtype = 2, mod = 1, price = 1500},
+  {name = "Triplegolds", wtype = 2, mod = 2, price = 1500},
+  {name = "Bigworm", wtype = 2, mod = 3, price = 1500},
+  {name = "Sevenfives", wtype = 2, mod = 4, price = 1500},
+  {name = "Splitsix", wtype = 2, mod = 5, price = 1500},
+  {name = "Freshmesh", wtype = 2, mod = 6, price = 1500},
+  {name = "Leadsled", wtype = 2, mod = 7, price = 1500},
+  {name = "Turbine", wtype = 2, mod = 8, price = 1500},
+  {name = "Superfin", wtype = 2, mod = 9, price = 1500},
+  {name = "Classicrod", wtype = 2, mod = 10, price = 1500},
+  {name = "Dollar", wtype = 2, mod = 11, price = 1500},
+  {name = "Dukes", wtype = 2, mod = 12, price = 1500},
+  {name = "Lowfive", wtype = 2, mod = 13, price = 1500},
+  {name = "Gooch", wtype = 2, mod = 14, price = 1500},
+}
+-----Muscle wheels-----
+cfg.musclewheels = {
+  {name = "Stock", wtype = 1, mod = -1, price = 500},
+  {name = "Classicfive", wtype = 1, mod = 0, price = 500},
+  {name = "Dukes", wtype = 1, mod = 1, price = 500},
+  {name = "Musclefreak", wtype = 1, mod = 2, price = 500},
+  {name = "Kracka", wtype = 1, mod = 3, price = 500},
+  {name = "Azrea", wtype = 1, mod = 4, price = 500},
+  {name = "Mecha", wtype = 1, mod = 5, price = 500},
+  {name = "Blacktop", wtype = 1, mod = 6, price = 500},
+  {name = "Dragspl", wtype = 1, mod = 7, price = 500},
+  {name = "Revolver", wtype = 1, mod = 8, price = 500},
+  {name = "Classicrod", wtype = 1, mod = 9, price = 500},
+  {name = "Spooner", wtype = 1, mod = 10, price = 500},
+  {name = "Fivestar", wtype = 1, mod = 11, price = 500},
+  {name = "Oldschool", wtype = 1, mod = 12, price = 500},
+  {name = "Eljefe", wtype = 1, mod = 13, price = 500},
+  {name = "Dodman", wtype = 1, mod = 14, price = 500},
+  {name = "Sixgun", wtype = 1, mod = 15, price = 500},
+  {name = "Mercenary", wtype = 1, mod = 16, price = 500},
+}
+
+
+return cfg
---
+++ lscustoms_server.lua
@@ -0,0 +1,334 @@
+--[[
+Los Santos Customs V1.1
+Credits - MythicalBro
+/////License/////
+Do not reupload/re release any part of this script without my permission
+]]
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+-- RESOURCE TUNNEL/PROXY
+vRPLSs = {}
+Tunnel.bindInterface("vrp_lscustom",vRPLSs)
+Proxy.addInterface("vrp_lscustom",vRPLSs)
+ADVclient = Tunnel.getInterface("vrp_adv_garages")
+LSClient = Tunnel.getInterface("vrp_lscustom")
+
+
+local cfg = module("vrp_lscustom", "cfg/serverconfig")
+
+local tbl = {
+	[1] = {locked = false, player = nil},
+	[2] = {locked = false, player = nil},
+	[3] = {locked = false, player = nil},
+	[4] = {locked = false, player = nil},
+	[5] = {locked = false, player = nil},
+	[6] = {locked = false, player = nil},
+}
+RegisterServerEvent('lockGarage')
+AddEventHandler('lockGarage', function(b,garage)
+	tbl[tonumber(garage)].locked = b
+	if not b then
+		tbl[tonumber(garage)].player = nil
+	else
+		tbl[tonumber(garage)].player = source
+	end
+	TriggerClientEvent('lockGarage',-1,tbl)
+	--print(json.encode(tbl))
+end)
+RegisterServerEvent('getGarageInfo')
+AddEventHandler('getGarageInfo', function()
+	TriggerClientEvent('lockGarage',-1,tbl)
+	--print(json.encode(tbl))
+end)
+AddEventHandler('playerDropped', function()
+	for i,g in pairs(tbl) do
+		if g.player then
+			if source == g.player then
+				g.locked = false
+				g.player = nil
+				TriggerClientEvent('lockGarage',-1,tbl)
+			end
+		end
+	end
+end)
+
+function checkModsPrice(button)
+	if button.modtype ~=nil and button.mod ~= nil then
+		local modtype = button.modtype
+		local modindex = button.mod
+		local wtype = button.wtype
+		--print("button: "..vRP.dump(button))
+		--print(modtype .. "/////" .. modindex)
+
+		if modtype == 22 or modtype == 18 or modtype == 16 or modtype == 15 or modtype == 14 or modtype == 13 or modtype == 12 or modtype == 11 then
+			for c,v in ipairs(cfg.mods[modtype]) do
+				if v.mod == modindex then
+					value = v.price
+				end
+			end
+		elseif modtype == 24 and wtype then
+			if wtype == 6 then
+				for w,t in pairs(cfg.backwheel) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			end
+		elseif modtype == 23 and wtype then
+			if wtype == 0 then
+				for w,t in pairs(cfg.sportwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 1 then
+				for w,t in ipairs(cfg.musclewheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 2 then
+				for w,t in ipairs(cfg.lowriderwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 3 then
+				for w,t in ipairs(cfg.suvwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 4 then
+				for w,t in ipairs(cfg.offroadwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 5 then
+				for w,t in ipairs(cfg.tunerwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 6 then
+				for w,t in ipairs(cfg.frontwheel) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			elseif wtype == 7 then
+				for w,t in ipairs(cfg.highendwheels) do
+					if t.mod == modindex then
+						value = t.price
+					end
+				end
+			end
+		else
+			--print("CAIU AQUI SAPORRA?")
+			if modindex == -1 then
+				value = 0
+			else
+				value = cfg.mods[modtype].startprice
+			end
+		end
+	else
+		value = 0
+	end
+	return value
+end
+
+function toogleTrue(value)
+	if value == 1 then
+		value = true
+	end
+	return value
+end
+
+function vRPLSs.saveCustomsVeh(veh)
+	local user_id = vRP.getUserId(source)
+	local player = vRP.getUserSource(user_id)
+	if user_id ~= nil then
+		local model = veh.model --Display name from vehicle model(comet2, entityxf)
+		local vehid = veh.vehicle -- get id vehicle
+		local plate = veh.plate
+		local mods = veh.mods
+
+		local vtype, vehname = ADVclient.isOwnedVehicleOutID(player, vehid)
+
+		if vtype and vehname then
+			local custom = {}
+
+			custom.plate = {}
+			custom.plate.text = plate -- Licence ID
+			custom.plate.index = veh.plateindex
+
+			custom.colour = {}
+			custom.colour.primary = veh.color[1] -- 1rst colour
+			custom.colour.secondary = veh.color[2] -- 2nd colour
+			custom.colour.pearlescent = veh.extracolor[1] -- colour type
+			custom.colour.wheel = veh.extracolor[2] -- wheel colour
+			-- colors[1] = custom.colour.primary
+			-- colors[2] = custom.colour.secondary
+			-- colors[3] = custom.colour.pearlescent
+			-- colors[4] = custom.colour.wheel
+			custom.neon = {}
+			custom.neon.left = veh.neon.left
+			custom.neon.right = veh.neon.right
+			custom.neon.front = veh.neon.front
+			custom.neon.back = veh.neon.back
+
+			custom.colour.neon = veh.neoncolor
+			custom.colour.smoke = veh.smokecolor
+			--
+			-- custom.colour.custom = {}
+			-- custom.colour.custom.primary = table.pack(GetVehicleCustomPrimaryColour(veh))
+			-- custom.colour.custom.secondary = table.pack(GetVehicleCustomSecondaryColour(veh))
+			--
+			custom.mods = {}
+			for i=0,48 do
+				custom.mods[i] = tonumber(mods[i].mod)
+			end
+
+			-- custom.mods[18] = IsToggleModOn(veh,18)
+			-- custom.mods[20] = IsToggleModOn(veh,20)
+			-- custom.mods[22] = IsToggleModOn(veh,22)
+			--
+			-- mods = custom.mods
+			--
+			-- custom.mods[18] = mods[18].mod -- bool
+			-- custom.mods[20] = mods[20].mod -- bool
+			-- custom.mods[22] = mods[22].mod -- bool
+			custom.mods[46] = veh.windowtint -- Tinted Windows
+			--
+			-- custom.neon = {}
+			-- custom.neon.left = IsVehicleNeonLightEnabled(veh,0)
+			-- custom.neon.right = IsVehicleNeonLightEnabled(veh,1)
+			-- custom.neon.front = IsVehicleNeonLightEnabled(veh,2)
+			-- custom.neon.back = IsVehicleNeonLightEnabled(veh,3)
+			--
+			-- custom.bulletproof = GetVehicleTyresCanBurst(veh)
+			custom.variation = mods[23].mod
+			custom.wheel = veh.wheeltype -- Wheel Type
+			-- wheel = custom.wheel
+
+			--print("veh: ".. json.encode(custom))
+
+			vRPclient._notify(player, "Customizações foram Salvos")
+			vRP.setSData("custom:u"..user_id.."veh_"..vehname, json.encode(custom))
+		end
+
+		--[[
+		mods[0].mod - spoiler
+		mods[1].mod - front bumper
+		mods[2].mod - rearbumper
+		mods[3].mod - skirts
+		mods[4].mod - exhaust
+		mods[5].mod - roll cage
+		mods[6].mod - grille
+		mods[7].mod - hood
+		mods[8].mod - fenders
+		mods[10].mod - roof
+		mods[11].mod - engine
+		mods[12].mod - brakes
+		mods[13].mod - transmission
+		mods[14].mod - horn
+		mods[15].mod - suspension
+		mods[16].mod - armor
+		mods[23].mod - tires
+		mods[23].variation - custom tires
+		mods[24].mod - tires(Just for bikes, 23:front wheel 24:back wheel)
+		mods[24].variation - custom tires(Just for bikes, 23:front wheel 24:back wheel)
+		mods[25].mod - plate holder
+		mods[26].mod - vanity plates
+		mods[27].mod - trim design
+		mods[28].mod - ornaments
+		mods[29].mod - dashboard
+		mods[30].mod - dial design
+		mods[31].mod - doors
+		mods[32].mod - seats
+		mods[33].mod - steering wheels
+		mods[34].mod - shift leavers
+		mods[35].mod - plaques
+		mods[36].mod - speakers
+		mods[37].mod - trunk
+		mods[38].mod - hydraulics
+		mods[39].mod - engine block
+		mods[40].mod - cam cover
+		mods[41].mod - strut brace
+		mods[42].mod - arch cover
+		mods[43].mod - aerials
+		mods[44].mod - roof scoops
+		mods[45].mod - tank
+		mods[46].mod - doors
+		mods[48].mod - liveries
+
+		--Toggle mods
+		mods[20].mod - tyre smoke
+		mods[22].mod - headlights
+		mods[18].mod - turbo
+
+		--]]
+		-- local color = veh.color
+		-- local extracolor = veh.extracolor
+		-- local neoncolor = veh.neoncolor
+		-- local smokecolor = veh.smokecolor
+		-- local plateindex = veh.plateindex
+		-- local windowtint = veh.windowtint
+		-- local wheeltype = veh.wheeltype
+		-- local bulletProofTyres = veh.bulletProofTyres
+		--Do w/e u need with all this stuff when vehicle drives out of lsc
+	end
+end
+
+RegisterServerEvent("LSC:finished")
+AddEventHandler("LSC:finished", function(veh)
+
+	local user_id = vRP.getUserId(source)
+	local player = vRP.getUserSource(user_id)
+	--print(player)
+	if user_id ~= nil then
+		local model = veh.model --Display name from vehicle model(comet2, entityxf)
+		local vehid = veh.vehicle -- get id vehicle
+		local plate = veh.plate
+		local mods = veh.mods
+
+		local vtype, vehname = ADVclient.isOwnedVehicleOutID(player, vehid)
+
+		if vtype and vehname then
+			vRPLSs.saveCustomsVeh(veh)
+		end
+	end
+end)
+
+RegisterServerEvent("LSC:buttonSelected")
+AddEventHandler("LSC:buttonSelected", function(name, button, veh)
+	local user_id = vRP.getUserId(source)
+	local player = vRP.getUserSource(user_id)
+	local model = veh.model or false
+	local vehid = veh.vehicle
+	local price = button.price
+	local mod = button.mod
+	if user_id ~= nil then
+		if price ~= nil and model then
+			local priceserv = checkModsPrice(button)
+			if (price >= priceserv and mod) or (not mod) then -- check if button have price
+				local vtype, vehname = ADVclient.isOwnedVehicleOutID(player, vehid)
+				if vtype and vehname then
+					if vRP.tryFullPayment(user_id,price) then
+						TriggerClientEvent("LSC:buttonSelected", player,name, button, true)
+						--saveCustomsVeh(user_id,vehname,veh)
+					else
+						TriggerClientEvent("LSC:buttonSelected", player,name, button, false)
+					end
+				else
+					TriggerClientEvent("LSC:buttonSelected", player,name, button, false)
+					vRPclient._notify(player, "Este veículo não é seu, não é possivel customizar!")
+				end
+			end
+		end
+	end
+end)
